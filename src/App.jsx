dea8a358836f066aa578c1bf1f24f6bec1b1a7d6
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-08-19 13:50:47 -0700

    Fixed corse url

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { SignupPage } from "./SignupPage";
 import { ProductsNewPage } from "./ProductsNewPage";
 import { Home } from "./Home";
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mini-capstone-api-97g1.onrender.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mini-capstone-api-dfdz.onrender.com";
 
 const router = createBrowserRouter([
   {
